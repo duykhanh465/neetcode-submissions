9ab46bc153487385282fbe61a235d3dcf7b9075f
duykhanh465 <khanh040605@gmail.com>
2026-07-26 09:40:18 +0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    isValid(s) {
+        const stack = [];
+        const pairs = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+
+        for (let char of s) {
+            if (char === '(' || char === '{' || char === '[') {
+                stack.push(char);
+            } else {
+                if (stack.length === 0 || stack.pop() !== pairs[char]) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
+
+module.exports = Solution;
